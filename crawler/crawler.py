05d093f130b6f571c56d1ef76268690a1613da41
purvik-n <purviknarasimha20@gmail.com>
2026-04-12 22:42:59 +0530

    Reorganize crawler.py

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -5,3 +5,4 @@ import sys
 # Revisited on 2026-04-08T15:18:51
 # TODO: Expand on this section later.
 # Note: formatting checked at 2026-04-12T11:54:33
+# Note: formatting checked at 2026-04-12T22:42:59
