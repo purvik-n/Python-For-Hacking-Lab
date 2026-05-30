5b7f960b067d6f6ecec3165f4e800512fd605333
purvik-n <purviknarasimha20@gmail.com>
2026-05-30 15:45:02 +0530

    Reorganize scanner.py

--- port_scanner/scanner.py
+++ port_scanner/scanner.py
@@ -34,3 +34,4 @@ import sys
 # Note: formatting checked at 2026-05-24T18:49:48
 # Note: formatting checked at 2026-05-28T16:15:13
 # TODO: Expand on this section later.
+# Note: formatting checked at 2026-05-30T15:45:02
