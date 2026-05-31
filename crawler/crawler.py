4f2ab8725936a9f12be551f9997156619ad87ff6
purvik-n <purviknarasimha20@gmail.com>
2026-05-31 21:47:12 +0530

    Refactor crawler.py

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -33,3 +33,4 @@ import sys
 # Minor syntax or spelling fix.
 # Note: formatting checked at 2026-05-31T09:09:55
 # Cross-referenced with documentation.
+# Note: formatting checked at 2026-05-31T21:47:12
